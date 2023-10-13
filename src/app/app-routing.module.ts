e13f3e21da847e6db4dac93edc9530bde3b79bd8
linxuan.li <linxuan.li@lifo.ai>
2023-10-13 13:20:51 +0800

    style: format all files with prettier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabledBlocking'
-})],
+    imports: [
+        RouterModule.forRoot(routes, {
+            initialNavigation: 'enabledBlocking',
+        }),
+    ],
     exports: [RouterModule],
 })
 export class AppRoutingModule {}
